e203fa3b798ac87c1a0dacea02ddf3d043ec6817
Wen Dong <dong23@uwindsor.ca>
2021-05-20 21:14:11 +0800

    t

--- networking_mon/server2.py
+++ networking_mon/server2.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 from socket import *
 from _thread import *
-serverName = "10.211.36.170"
+serverName = "137.207.82.53"
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind((serverName, serverPort))
