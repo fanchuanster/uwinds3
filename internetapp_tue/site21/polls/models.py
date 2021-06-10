f2dde6caf3c5148cca0d9a8366d25f97ff6b51bf
Wen Dong <fanchuanster@gmail.com>
2021-06-10 08:35:06 +0800

    t

--- internetapp_tue/site21/polls/models.py
+++ internetapp_tue/site21/polls/models.py
@@ -8,13 +8,13 @@ from django.utils import timezone
 
 class Topic(models.Model):
     def __str__(self):
-        return f"{self.name}, {self.length}"
+        return f"{self.name}"
     name = models.CharField(max_length=200)
     length = models.BigIntegerField(default=12)
 
 class Course(models.Model):
     def __str__(self):
-        return f"{self.title}, {self.topic}, {self.price}"
+        return f"{self.title}"
     title = models.CharField(max_length=200)
     topic = models.ForeignKey(Topic, related_name='courses', on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
