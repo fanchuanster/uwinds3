65550f9c434744f66b57553e73500432be99b489
Wen Dong <fanchuanster@gmail.com>
2021-06-17 18:28:42 +0800

    t

--- challenge/main.py
+++ challenge/main.py
@@ -17,23 +17,33 @@ x_df = df.iloc[:, :-2].astype(np.float)
 y_df = df.iloc[:, -2:].astype(np.int64)
 
 cordf = x_df.corr().abs()
-cordf = cordf.replace(1.0, 0.0)
+print(range(cordf.shape[1]))
+for i in range(cordf.shape[1]):
+    print(i)
 
 threshhold = 0.94
 todrop = defaultdict(list)
-for column in cordf:
-    s = cordf[column]
-    if any(s > threshhold):
-        for i, v in s.items():
-            if v > threshhold:
-                if i not in todrop:
-                    todrop[column].append((i, v))
+for i in range(cordf.shape[0]):
+    for j in range(cordf.shape[1]):
+        v = cordf.iat[i, j]
+        if v > threshhold and i != j and j not in todrop:
+            todrop[i].append((j, v))
+            
+print(todrop)
 todroplist = list(set([item[0] for k,v in todrop.items() for item in v]))
+print(len(todroplist), todroplist)
 
-x_df = x_df.drop(todroplist, axis=1)
-
+x_df = x_df.drop(x_df.columns[todroplist], axis=1)
+x_df.head(10)
 y_df['Label'] = y_df.apply(lambda r: r['Label1'] * (-1 if r['Label2'] == 0 else 1), axis=1)
-
+y_df = y_df['Label']
+y_df.head(10)
 for column in y_df:
     col = y_df[column]
     print(column, pd.unique(col), col.min(), col.mean(), col.max())
+
+for column in x_df:
+    col = x_df[column]
+    print(column, pd.unique(col), col.min(), col.mean(), col.max())
+
+
