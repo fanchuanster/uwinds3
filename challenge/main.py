9c804dd046c69a2e96ca5206488cab851039a3fa
Wen Dong <fanchuanster@gmail.com>
2021-06-18 22:31:14 +0800

    t

--- challenge/main.py
+++ challenge/main.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 from collections import defaultdict
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as tfk
 
 # exact match ratio
 #MR = np.all(y_pred == y_true, axis=1).mean()
@@ -26,35 +29,72 @@ def remove_correlations(df, threshold = 0.94):
     df.drop(df.columns[todroplist], axis=1, inplace=True)
     return df
 
+def remove_sparses(df, threshold = 0.89):
+    sparse_columns = []
+    for column in df:
+        col = df[column]
+        highest = col.value_counts().iat[0]
+        if highest / len(df.index) > threshold:
+            print(column, col.value_counts().index[0], col.value_counts().iat[0], highest / len(x_df.index))
+            sparse_columns.append(column)
+    df.drop(sparse_columns, axis=1, inplace=True)
+    return df
+
+
 df = pd.read_csv("Train.csv")
 df = remove_correlations(df)
-df.columns
-x_df = df.iloc[:, :-2].astype(np.float)
-y_df = df.iloc[:, -2:].astype(np.int64)
+df = remove_sparses(df)
 
-x_df.head(10)
-y_df['Label'] = y_df.apply(lambda r: r['Label1'] * (-1 if r['Label2'] == 0 else 1), axis=1)
+df_7 = df[df['Label1'] == 7]
+
+x_df = df.iloc[:, :-2].astype('float32')
+y_df = df.iloc[:, -2:].astype('int32')
+
+sc = preprocessing.StandardScaler()
+x = sc.fit_transform(x_df)
+
+y_df['Label'] = y_df.apply(lambda r: r['Label1'] * (-1 if r['Label2'] == 0 else 1), axis=1).astype(np.int64)
+y_df = y_df[['Label1']]
+for column in y_df:
+    col = y_df[column]
+    print(column, '\n', col.value_counts())
+    
+ y = tfk.utils.to_categorical(y_df, dtype="int32")
+ y_df.columns
+ y[:10]
+print(np.unique(y))
+
+X_train, X_test, y_train, y_test = train_test_split(x, y_df.to_numpy(), test_size=0.2, random_state=0)
+print(X_train.shape)
+print(y_train.shape)
 
 for column in x_df:
     col = x_df[column]
-    if len(pd.unique(col)) < 100:
+    if len(pd.unique(col)) < 1000:
         print(column, type(col.value_counts()), '\n', col.value_counts())
+        
 
-for column in x_df:
-    col = x_df[column]
-    highest = col.value_counts().iat[0]
-    if highest / len(x_df.index) > 0.9:
-        print(col.value_counts().iat[0])
-        #print(column, type(col.value_counts()), '\n', col.value_counts())
+# create model
+model = tfk.Sequential()
+model.add(tfk.layers.Dense(50,input_shape=(26,), activation='relu')) #First Hidden Layer
+model.add(tfk.layers.Dense(100, activation='relu')) #Second  Hidden Layer
+model.add(tfk.layers.Dense(11, activation='softmax')) #Output Layer
+#model Looks like:  784 input -> [50 units in layer1] ->[100 units in layer2] -> 1 output
+
+# Compiling the model  
+model.compile(optimizer='adam',                         
+              loss='sparse_categorical_crossentropy',
+              metrics=['sparse_categorical_accuracy'])
+#Model's Summary
+model.summary()
 
 
-y_df = y_df['Label1']
-y_df.head(-10)
+for i in [X_train, X_test, y_train, y_test]:
+    print(type(i))
 
-x_df.columns
-
-def main():
-    pass
+#Training the model 
+training = model.fit(X_train, y_train, epochs = 20, batch_size = 5000, validation_data = (X_test, y_test))
+        
 
 if __name__ == '__main__':
     main()
