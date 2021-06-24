e08d921efd960bac07e229f302eae3e4849a9b59
Wen Dong <fanchuanster@gmail.com>
2021-06-24 20:52:35 +0800

    t

--- challenge/Coding/cpp/main.cpp
+++ challenge/Coding/cpp/main.cpp
@@ -15,7 +15,7 @@ class BaseClass
   public:
     BaseClass(int i, const string& s):i(i), s(s)
     {}
-    explict BaseClass(int i):BaseClass(i,"dummy")
+    explicit BaseClass(int i):BaseClass(i,"dummy")
     {
     }
     void expr() const
