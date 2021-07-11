aec7f0312b16c8de28927ce1c3abef6598035904
Wen Dong <fanchuanster@gmail.com>
2021-07-11 11:18:10 +0800

    t

---
+++ networking_mon/lab6/endec_AES.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+from Crypto.Cipher import AES
+from Crypto.Util import Padding
+from Crypto.Random import get_random_bytes
+
+def encryptRSA(message, keyfile):
+    key=RSA.importKey(open(keyfile).read())
+
+    cipher=PKCS1_OAEP.new(key)
+
+    ciphertext=cipher.encrypt(message)
+
+key_hex_string = '00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF'
+key = bytes.fromhex(key_hex_string)
+iv = encryptRSA(get_random_bytes(16), 'public.pem')
+
+data = b'I find the solution for P not equal NP'
+print(data.hex())
+
+# Encrypt the entire data
+cipher = AES.new(key, AES.MODE_CBC, iv)
+ciphertext = cipher.encrypt(Padding.pad(data, 16))
+print("Ciphertext: {0}\n".format(ciphertext.hex()))
+
+# Decrypt the ciphertext
+cipher = AES.new(key, AES.MODE_CBC, iv)
+plaintext = cipher.decrypt(ciphertext)
+print("Plaintext: {0}".format(Padding.unpad(plaintext, 16)))
+
