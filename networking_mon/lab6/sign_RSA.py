6db7ac4cfcaa0f411a6fd7ad095e3c0135c4a83e
Wen Dong <fanchuanster@gmail.com>
2021-07-11 10:45:56 +0800

    t

---
+++ networking_mon/lab6/sign_RSA.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from Crypto.Signature import pss
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+
+message=b'I owe you $2000'
+key_str=open("private.pem").read()
+key=RSA.importKey(key_str, passphrase="111111")
+h=SHA256.new(message)
+print("hexdigest - '{}'".format(message), h.hexdigest())
+signer=pss.new(key)
+sig=signer.sign(h)
+open("signature.bin", "wb").write(sig)
+
+
