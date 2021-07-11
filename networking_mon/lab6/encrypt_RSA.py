eb66e5781e0902850a3fc463588167d928b19ac2
Wen Dong <fanchuanster@gmail.com>
2021-07-11 10:18:45 +0800

    m

---
+++ networking_mon/lab6/encrypt_RSA.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+message=input("input the message you would like to encrypt:")
+
+key=RSA.import_key(open('public.pem').read())
+print(key)
+
+
+cipher=PKCS1_OAEP.new(key)
+
+ciphertext=cipher.encrypt(message)
+
+f=open('ciphertext.bin', 'wb')
+
+f.write(ciphertext)
+
+f.close()
