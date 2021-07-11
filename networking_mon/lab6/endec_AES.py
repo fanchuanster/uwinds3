8e6c9677a5657b7e26b34b2131f7f76b9bfd5414
Wen Dong <fanchuanster@gmail.com>
2021-07-11 11:50:16 +0800

    t

--- networking_mon/lab6/endec_AES.py
+++ networking_mon/lab6/endec_AES.py
@@ -1,26 +1,50 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 from Crypto.Util import Padding
 from Crypto.Random import get_random_bytes
 
 import binascii
 
+def encryptRSA(message, keyfile):
+    key=RSA.importKey(open(keyfile).read())
+    cipher=PKCS1_OAEP.new(key)
+    ciphertext=cipher.encrypt(message)
+
+def decryptRSA(ciphertext, privatekeyfile):
+    key_str = open(privatekeyfile).read()
+    prikey = RSA.importKey(key_str, passphrase='111111')
+    cipher = PKCS1_OAEP.new(prikey)
+    message = cipher.decrypt(ciphertext)
+    return message
+
+
 key_hex_string = '00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF'
 key = bytearray.fromhex(key_hex_string)
-iv = get_random_bytes(16)
+
+# a.
+iv = get_random_bytes(32)
+
+# b.
+c1 = encryptRSA(iv, "public.pem")
 
 data = b'I find the solution for P not equal NP'
 print(data.decode())
 
-# Encrypt the entire data
+# c.
 cipher = AES.new(key, AES.MODE_CBC, iv)
-ciphertext = cipher.encrypt(Padding.pad(data, 16))
-print("Ciphertext: {0}\n".format(binascii.hexlify(bytearray(ciphertext))))
+c2 = cipher.encrypt(Padding.pad(data, 16))
+print("c2: {0}\n".format(binascii.hexlify(bytearray(c2))))
 
+
+# d.
+decrpted_message = decryptRSA(c1, "private.pem")
+print("decrypted c1", decrpted_message)
 
 # Decrypt the ciphertext
 cipher = AES.new(key, AES.MODE_CBC, iv)
-plaintext = cipher.decrypt(ciphertext)
-print("Plaintext: {0}".format(Padding.unpad(plaintext, 16)))
+plaintext = cipher.decrypt(c2)
+print("Plaintext c2 {0}".format(Padding.unpad(plaintext, 16)))
 
