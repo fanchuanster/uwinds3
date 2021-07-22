2bf122960ac743960bf51de5709513ce4c1259c7
Wen Dong <wen.dong@microfocus.com>
2021-07-22 09:02:27 +0800

    t

--- internetapp_tue/site21/polls/admin.py
+++ internetapp_tue/site21/polls/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Topic, Course, Student, Order
+from .models import Topic, Course, Student, Order, Review
 # Register your models here. admin.site.register(Topic)
 admin.site.register(Course)
 admin.site.register(Topic)
 admin.site.register(Student)
+admin.site.register(Review)
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
