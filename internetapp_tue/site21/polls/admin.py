525f52533f8c19d60b4863185b0099284a700989
Wen Dong <wen.dong@microfocus.com>
2021-08-05 04:13:43 +0800

    t

--- internetapp_tue/site21/polls/admin.py
+++ internetapp_tue/site21/polls/admin.py
@@ -3,11 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Topic, Course, Student, Order, Review
-# Register your models here. admin.site.register(Topic)
-# admin.site.register(Course)
-admin.site.register(Topic)
-admin.site.register(Student)
-admin.site.register(Review)
+# admin.site.register(Topic)
+# admin.site.register(Student)
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -18,4 +15,18 @@ class OrderAdmin(admin.ModelAdmin):
 class CourseAdmin(admin.ModelAdmin):
     fields = [('title', 'topic'), ('price', 'num_reviews')]
     list_display = ('title', 'topic', 'price')
-    # change_list_template =
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    fields = ['reviewer', 'course', 'rating', 'comments', 'date']
+    list_display = ('reviewer', 'course', 'rating', 'comments', 'date')
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    fields = ['name', 'length']
+    list_display = ('name', 'length')
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    fields = ['level', 'address', 'province', 'registered_courses', 'interested_in']
+    list_display = ('username', 'level', 'address', 'province')
